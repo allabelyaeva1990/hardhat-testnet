998baf8c812d3f8444e29bef947a693f9ac78124
Alla Belayeva <allabelyaeva1990@gmail.com>
2025-09-11 13:23:34 +0300

    Initial hardhat testnet setup

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  networks: {
+    hardhat: {
+      // Встроенная сеть Hardhat
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545", // Адрес вашей локальной ноды
+    },
+  },
+};
